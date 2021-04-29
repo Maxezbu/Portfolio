ef9b77a82a87becc8640c0be986b0d152f348309
Maxezbu <ezeburru@gmail.com>
2021-04-29 12:50:59 -0300

    primeros cambios

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,10 +9,10 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: ' ',
+  name: 'Ezequiel Buruchaga',
+  subtitle: 'Full-Stack Web Developer',
+  cta: 'Saber más',
 };
 
 // ABOUT DATA
@@ -21,19 +21,20 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1JbZ87-5CFaBmP31HockzxNbsfUQ1Ivk2/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'admin&&cadeteria.png',
+    title: 'APP - SERVICIO DE ENVÍOS A DOMICILIO',
+    info:
+      'Aplicación para empresas que utilizan el envio de paquetes a domicilio a través del servicio de cadeterías. La empresa sube los pedidos por día y son repartidos a las cadeterías que estan habilitadas. A su vez cada cadetería registra/habilita a sus cadetes para tomar las ordenes que cuentan con ubicación de entrega (map) y otros datos específicos.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/Maxezbu/Cruce', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -57,7 +58,7 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
+  cta: 'Podés encontrarme en LinkedIn o GitHub',
   btn: '',
   email: '',
 };
@@ -65,7 +66,7 @@ export const contactData = {
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
+    /*   {
       id: nanoid(),
       name: 'twitter',
       url: '',
@@ -74,16 +75,16 @@ export const footerData = {
       id: nanoid(),
       name: 'codepen',
       url: '',
-    },
+    }, */
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/ezequiel-buruchaga',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/Maxezbu',
     },
   ],
 };
